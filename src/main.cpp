5737101ac809f7065aff721bd59f83d0e4787b26
AidaMillatiMardiana <aidadiana2702@gmail.com>
2023-03-21 16:19:39 +0700

    Pertemuan 6 IOT

---
+++ src/main.cpp
@@ -0,0 +1,56 @@
+    #include <Arduino.h>
+    #include <SimpleDHT.h>
+
+    #define pinDHT 7 // SD3 pin signal sensor DHT
+
+    byte temperature = 0;
+    byte humidity = 0;
+
+    SimpleDHT11 dht11(D1); //instan sensor dht11
+
+    void printTemperature(float tempc)
+    {
+        float tempR = tempc * 0.8; // konversi ke reamur
+        float tempK = tempc + 273.15; // konversi ke reamur
+        
+        Serial.print("Temperature: ");
+        Serial.print(tempc);
+        Serial.print(" *C, ");
+        Serial.print(tempR);
+        Serial.print(" *R. ");
+        Serial.print(tempK);
+        Serial.print(" *K. ");
+        
+    }
+    void KelembabanSuhu()
+    {
+        int err = SimpleDHTErrSuccess;
+
+        if ((err = dht11.read(&temperature, &humidity, NULL)) != SimpleDHTErrSuccess)
+        {
+            Serial.print("Pembacaan DHT11 gagal, err=");
+            Serial.println(err);
+            delay(1000);
+            return;
+        }
+
+        Serial.print("Sample OK: ");
+        printTemperature((float)temperature);
+
+        Serial.print((int)humidity);
+        Serial.println(" H");
+
+        delay(1500);
+    }
+
+    void setup()
+    {
+        Serial.begin(115200);
+        Serial.println("Simple DHT");
+        delay(1000);
+    }
+
+    void loop()
+    {
+        KelembabanSuhu();
+    }
